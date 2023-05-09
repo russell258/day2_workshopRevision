ec862939d53103d75e40b0435b1c5f269b4facba
russell258 <duckie258@gmail.com>
2023-05-09 16:01:31 +0800

    2nd revision chiong

--- src/main/java/sg/nus/edu/iss/App.java
+++ src/main/java/sg/nus/edu/iss/App.java
@@ -4,33 +4,8 @@ package sg.nus.edu.iss;
 public class App 
 {
     public static void main( String[] args ){
-
-        //create bankacct class
-
-        //instantiate bankAcct class into one object here
-        //call the get or set method to test
-        // test the methods
-        BankAccount bank1 = new BankAccount("Russell", 3000.00f);  
-        bank1.deposit(20.00f);
         
 
-        bank1.withdraw(50.00f);
-
-        System.out.println(bank1.toString());
-
-        FixedDepositAccount account2 = new FixedDepositAccount("Meow Meow", 2000.00f, 5, 18);
-        System.out.println("Account Balance of fixed deposit =" + account2.getAccountBalance());
-        account2.withdraw(100.0f);
-        System.out.println("Account Balance of fixed deposit =" + account2.getAccountBalance());
-        account2.deposit(100.0f);
-        System.out.println("Account Balance of fixed deposit =" + account2.getAccountBalance());
-
-        System.out.println(account2.toString());
-        account2.setInterest(6);
-        //account2.setInterest(10);
-
-        account2.setDuration(18);
-        account2.setDuration(10);
 
     }
 }
--- src/main/java/sg/nus/edu/iss/BankAccount.java
+++ src/main/java/sg/nus/edu/iss/BankAccount.java
@@ -1,59 +1,37 @@
 package sg.nus.edu.iss;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.UUID;
-
-//create getter setter
-// create constructor n params
-// overload?
-// override
+import java.sql.Date;
+import java.util.List;
 
 public class BankAccount {
-    private String accountName;
-    private String accountNumber; //randomly genereated
-    private float accountBalance; // check whether to use primitive or wrapper
-    private ArrayList<String> accountTransactions; //should be arraylist cos will add on or append
+    private String name;
+    private String accountNumber;
+    private float accountBalance;
+    private List<String> accountTransactions;
     private boolean isClosed;
     private Date accountCreatedDate;
     private Date accountClosedDate;
 
-    public BankAccount(){
-    }
-
-    public BankAccount(String accountName) {
-        this.accountName = accountName;
-        this.accountBalance = 0.0f;
-        this.isClosed = false;
-        this.accountTransactions = new ArrayList<>();
-        this.accountNumber = UUID.randomUUID().toString();
-        java.util.Date date = new java.util.Date();
-        this.setAccountCreatedDate(date);
-    }
-
-    public BankAccount(String accountName,  Float accountBalance) {
-        this.accountName = accountName;
+    public BankAccount(String name, String accountNumber, float accountBalance, List<String> accountTransactions,
+            boolean isClosed, Date accountCreatedDate, Date accountClosedDate) {
+        this.name = name;
+        this.accountNumber = accountNumber;
         this.accountBalance = accountBalance;
-        this.isClosed = false;
-        this.accountTransactions = new ArrayList<>();
-        this.accountNumber = UUID.randomUUID().toString().toUpperCase();
-        java.util.Date date = new java.util.Date();
-        this.setAccountCreatedDate(date);
+        this.accountTransactions = accountTransactions;
+        this.isClosed = isClosed;
+        this.accountCreatedDate = accountCreatedDate;
+        this.accountClosedDate = accountClosedDate;
     }
 
-
-    public String getAccountName() {
-        return accountName;
+    public String getName() {
+        return name;
     }
-
-    // accountName cannot be set once it has been set in creation so remove it
-    // public void setAccountName(String accountName) {
-    //     this.accountName = accountName;
+    // public void setName(String name) {
+    //     this.name = name;
     // }
     public String getAccountNumber() {
         return accountNumber;
     }
-    // accountNumber cannot be set once it has been set in creation so remove it
     // public void setAccountNumber(String accountNumber) {
     //     this.accountNumber = accountNumber;
     // }
@@ -63,19 +41,17 @@ public class BankAccount {
     public void setAccountBalance(float accountBalance) {
         this.accountBalance = accountBalance;
     }
-    public ArrayList<String> getAccountTransactions() {
+    public List<String> getAccountTransactions() {
         return accountTransactions;
     }
-
-    public boolean getIsClosed() {
+    public void setAccountTransactions(List<String> accountTransactions) {
+        this.accountTransactions = accountTransactions;
+    }
+    public boolean isClosed() {
         return isClosed;
     }
-    public void setIsClosed(boolean isClosed) {
+    public void setClosed(boolean isClosed) {
         this.isClosed = isClosed;
-        if (this.isClosed){
-            java.util.Date date = new java.util.Date();
-            this.setAccountClosedDate(date);
-        }
     }
     public Date getAccountCreatedDate() {
         return accountCreatedDate;
@@ -90,40 +66,11 @@ public class BankAccount {
         this.accountClosedDate = accountClosedDate;
     }
 
-    //deposit method
-    public void deposit(float depositAmount){
-        if (depositAmount<0){
-            System.out.println("Please enter a valid amount");
-        }else if (isClosed){
-            throw new IllegalArgumentException("Account was closed on "+this.getAccountClosedDate());
-        }else{
-            this.accountBalance += depositAmount;
-            java.util.Date date = new java.util.Date();
-            this.accountTransactions.add(depositAmount + " SGD was deposited on " + date);
-            System.out.println(depositAmount + " SGD was deposited on " + date);
-        }
-    }
-    
-    //withdraw method
-    public void withdraw(float withdrawAmount){
-        if (withdrawAmount<0){
-            System.out.println("Please enter a valid amount");
-        }else if (isClosed || withdrawAmount>this.accountBalance){
-            throw new IllegalArgumentException("Account was closed on "+ this.getAccountClosedDate());
-        }else{
-            this.accountBalance -= withdrawAmount;
-            java.util.Date date = new java.util.Date();
-            this.accountTransactions.add(withdrawAmount + " SGD was withdrawn on " + date);
-            System.out.println(withdrawAmount + " SGD was withdrawn on " + date);
-        }
-    }
-
     @Override
     public String toString() {
-        return "BankAccount [accountName=" + accountName + ", accountNumber=" + accountNumber + ", accountBalance="
-                + accountBalance + ", accountTransactions=" + accountTransactions + ", isClosed=" + isClosed
-                + ", accountCreatedDate=" + accountCreatedDate + ", accountClosedDate=" + accountClosedDate + "]";
+        return "BankAccount [name=" + name + ", accountNumber=" + accountNumber + ", accountBalance=" + accountBalance
+                + ", accountTransactions=" + accountTransactions + ", isClosed=" + isClosed + ", accountCreatedDate="
+                + accountCreatedDate + ", accountClosedDate=" + accountClosedDate + "]";
     }
-    
 
 }
